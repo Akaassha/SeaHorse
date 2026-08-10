30945e26bc6b01258668d633101afd727824b63b
Maria <materna.maria999@gmail.com>
2026-08-10 21:51:16 +0200

    Init

---
+++ Source/SeaHorse/SeaHorse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SeaHorse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SeaHorse, "SeaHorse" );
